b4a56c9d1a6e4d5293a718bad3a32929c7f8ccde
Atil Samancioglu <samancioglu.atil@gmail.com>
2019-07-12 23:48:17 +0300

    first commit

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var firstText: UITextField!
+    @IBOutlet weak var secondText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var result = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    
+        
     }
+    
+    
+    @IBAction func sumClicked(_ sender: Any) {
+        
+        
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+             
+                result = firstNumber + secondNumber
+                resultLabel.text = String(result)
+                
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func minusClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                result = firstNumber - secondNumber
+                resultLabel.text = String(result)
+                
+            }
+        }
+    }
+    
+    
+    @IBAction func multiplyClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                result = firstNumber * secondNumber
+                resultLabel.text = String(result)
+                
+            }
+        }
+    }
+    
+    
+    @IBAction func divideClicked(_ sender: Any) {
+        
+        if let firstNumber = Int(firstText.text!) {
+            if let secondNumber = Int(secondText.text!) {
+                
+                result = firstNumber / secondNumber
+                resultLabel.text = String(result)
+                
+            }
+        }
+        
+    }
+    
 
 
 }
